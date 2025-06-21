a2cb335df06afc83ded523eb1fa1d9fee9ffc7b5
Phillip Quiza <pquiza@gmail.com>
2025-06-21 12:17:48 +0100

    Another build process

--- Makefile
+++ Makefile
@@ -1,14 +1,26 @@
-build: embed-data
-	npx tailwindcss -i ./index.css -o ./static/css/index.css
+DIST=./dist
+STATIC_DIST=$(DIST)/static
+
+dist:
+	mkdir -p $(DIST)
+	mkdir -p $(STATIC_DIST)
+
+build: dist build-html
+	cp -R static $(DIST)
+	npx tailwindcss -i ./index.css -o $(STATIC_DIST)/css/index.css --minify
 
 watch:
-	npx tailwindcss -i ./index.css -o ./static/css/index.css --watch
+	npx tailwindcss -i ./index.css -o $(STATIC_DIST)/css/index.css --watch
 
 data:
 	node csv-to-json.js projects.csv > data.json
 
-embed-data: data
-	node inject-data.js
+build-html: data
+	cp index.html $(DIST)/index.html
+	node inject-data.js data.json $(DIST)/index.html
 
 clean:
 	rm data.json
+
+install:
+	npm install
--- inject-data.js
+++ inject-data.js
@@ -1,6 +1,30 @@
 const fs = require('fs')
-const data = fs.readFileSync('data.json', 'utf8')
-let html = fs.readFileSync('index.html', 'utf8')
-html = html.replace( '/* INJECT_JSON_DATA */', `${data}`)
-fs.writeFileSync('index.html', html)
 
+// Get filenames from command-line arguments
+const jsonFile = process.argv[2]
+const htmlFile = process.argv[3]
+
+// Helper to check if file exists and is readable
+function checkFile(file, desc) {
+  try {
+    fs.accessSync(file, fs.constants.R_OK)
+  } catch (err) {
+    console.error(`Error: ${desc} file "${file}" does not exist or is not readable.`)
+    process.exit(1)
+  }
+}
+
+// Check both files
+checkFile(jsonFile, 'JSON data')
+checkFile(htmlFile, 'HTML template')
+
+try {
+  const data = fs.readFileSync(jsonFile, 'utf8')
+  let html = fs.readFileSync(htmlFile, 'utf8')
+  html = html.replace('/* INJECT_JSON_DATA */', `${data}`)
+  fs.writeFileSync(htmlFile, html)
+  console.log('Successfully injected data into HTML.')
+} catch (err) {
+  console.error('Error while processing files:', err.message)
+  process.exit(1)
+}
